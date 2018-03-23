22c38c4ff7925371e219650f6439854b2138848d
Job Mos <jobmos95@gmail.com>
2018-03-23 10:37:58 +0100

    added intensity value calculation to main

--- processor.py
+++ processor.py
@@ -19,26 +19,23 @@ from calculateratio import calculateRatio
 from plotratio import plotRatio
 from calculateskycover import calculateSkyCover
 from setthresholds import setThresholds
+from calculateintensity import calculateIntensity
 
 # load the original image and displayi it
 # flag "1" indicates color image
 # flag "0" indicates greyscale image
 # flag "-1" indicates unchanged (?) image
-img = cv2.imread('processed_images/semi_clouds.jpg')
+img = cv2.imread('processed_images/broken_clouds.jpg')
 #cv2show(img,"Original image")
 
 # resolution of the image ( 352x288(x3) ) 
-# print(img.shape)
+#print(img.shape)
 
 # create mask
 mask = createmask(img)
 
 # apply the mask and display the result
 maskedImg = cv2.bitwise_and(img, mask)
-# cv2.namedWindow("Masked Image", cv2.WINDOW_NORMAL)
-# cv2.resizeWindow('Masked Image', 600,600)
-# cv2.imshow("Masked Image", maskedImg)
-# cv2.waitKey(0)
 
 # plot the overview showing the image, mask, and histogram
 #overviewPlot(img,mask,maskedImg)
@@ -47,18 +44,16 @@ maskedImg = cv2.bitwise_and(img, mask)
 sunnyThreshold,thinThreshold = setThresholds()
 
 # calculate red/blue ratio per pixel
-redBlueRatio = calculateRatio(img, maskedImg)
+redBlueRatio = calculateRatio(maskedImg)
+
+# calculate the intensity values
+intensityValues = calculateIntensity(maskedImg)
 
 # plot the reb/blue ratios
 #plotRatio(img,redBlueRatio, sunnyThreshold, thinThreshold)
-
-# can add:
-# if the average of all the ratios is > ... sunny
-# if the average of all the ratios is < ... cloudy
-# or vice versa
 
 # calculate solid angle corrections
 #calculateSACorrections(...)
 
 # calculate fractional skycover
-calculateSkyCover(redBlueRatio, sunnyThreshold, thinThreshold)
+#calculateSkyCover(redBlueRatio, sunnyThreshold, thinThreshold)
