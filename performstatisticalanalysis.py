768d33530a83c941fd8aeef932da47c4e12b5fa6
Job Mos <jobmos95@gmail.com>
2018-03-27 10:58:21 +0200

    moved GLCM to function, added average of 4 GLCM directions

--- performstatisticalanalysis.py
+++ performstatisticalanalysis.py
@@ -11,48 +11,13 @@
 #import libraries
 from myimports import *
 
-def performStatisticalAnalysis(maskedImg):
-
-	scaler = 16
-	greyLevels = int(256/scaler)
-
-	xres = 288
-	yres = 352
-
-	# extract R and B band individually from image
-	blueBand = np.zeros([yres,xres])
-	redBand = np.zeros([yres,xres])
-
-	for i in range (0,yres):
-		for j in range (0,xres):
-			# check whether on pixel is part of mask
-			# if pixel is masked, do nothing
-			if maskedImg[i,j,0] == 0 and maskedImg[i,j,1] == 0 and maskedImg[i,j,2] == 0:
-				pass
-			else:
-				# i = ypixel, j = xpixel, 0,2 = blue, red
-				blueBand[i,j] = int(maskedImg[i,j,0]/scaler)
-				redBand[i,j]  = int(maskedImg[i,j,2]/scaler)
-
-	plt.imshow(blueBand)
-	plt.show()
-
-	# SPECTRAL FEATURES
-
-	# mean (R and B)
-
-	# standard deviation (B)
-
-	# skewness (B)
-
-	# difference (R-G, R-B and G-B)
-
-	# TEXTURAL FEATURES
-
-	# Grey Level Co-occurrence Matrices (GLCM)
+def calculateGLCM(blueBand, greyLevels):
 	# matrix elements represent relative frequence that two pixels occur
 	# separated in a defined direction by distance dx,dy (A. Heinle et al., 2010)
-	GLCM = np.zeros([greyLevels,greyLevels], dtype = 'int')
+	GLCM1 = np.zeros([greyLevels,greyLevels], dtype = 'int')
+	GLCM2 = np.zeros([greyLevels,greyLevels], dtype = 'int')
+	GLCM3 = np.zeros([greyLevels,greyLevels], dtype = 'int')
+	GLCM4 = np.zeros([greyLevels,greyLevels], dtype = 'int')
 
 	# pixel distance
 	dx = 1
@@ -79,6 +44,7 @@ def performStatisticalAnalysis(maskedImg):
 			# loop over image pixels
 			for x in range (0, xres):
 				for y in range (0, yres):
+					# GLCM1
 					# check whether on pixel is part of mask
 					# if pixel is masked, do nothing
 					if blueBand[x,y] == 0 or blueBand[x+dx,y+dy] == 0:
@@ -86,12 +52,98 @@ def performStatisticalAnalysis(maskedImg):
 					else:
 						# when two pixels 'match' add +1 to GLCM matrix element
 						if blueBand[x,y] == i and blueBand[x+dx,y+dy] == j:
-							GLCM[i,j] += 1
+							GLCM1[i,j] += 1
 							#print(GLCM[i,j])
 						else:
 							pass
+					# GLCM2
+					# check whether on pixel is part of mask
+					# if pixel is masked, do nothing
+					if blueBand[x,y] == 0 or blueBand[x-dx,y+dy] == 0:
+						pass
+					else:
+						# when two pixels 'match' add +1 to GLCM matrix element
+						if blueBand[x,y] == i and blueBand[x-dx,y+dy] == j:
+							GLCM2[i,j] += 1
+							#print(GLCM[i,j])
+						else:
+							pass
+					# GLCM3
+					# check whether on pixel is part of mask
+					# if pixel is masked, do nothing
+					if blueBand[x,y] == 0 or blueBand[x+dx,y-dy] == 0:
+						pass
+					else:
+						# when two pixels 'match' add +1 to GLCM matrix element
+						if blueBand[x,y] == i and blueBand[x+dx,y-dy] == j:
+							GLCM3[i,j] += 1
+							#print(GLCM[i,j])
+						else:
+							pass
+					# GLCM4
+					# check whether on pixel is part of mask
+					# if pixel is masked, do nothing
+					if blueBand[x,y] == 0 or blueBand[x-dx,y-dy] == 0:
+						pass
+					else:
+						# when two pixels 'match' add +1 to GLCM matrix element
+						if blueBand[x,y] == i and blueBand[x-dx,y-dy] == j:
+							GLCM4[i,j] += 1
+							#print(GLCM[i,j])
+						else:
+							pass														
 
-	np.savetxt('test.txt', GLCM, fmt='%3d')
+	GLCM = (GLCM1+GLCM2+GLCM3+GLCM4)/4
+	
+	np.savetxt('GLCM1.txt', GLCM1, fmt='%3d')
+	np.savetxt('GLCM2.txt', GLCM2, fmt='%3d')
+	np.savetxt('GLCM3.txt', GLCM3, fmt='%3d')
+	np.savetxt('GLCM4.txt', GLCM4, fmt='%3d')
+
+	np.savetxt('GLCM.txt', GLCM, fmt='%3d')
+
+	return 0
+
+def performStatisticalAnalysis(maskedImg):
+
+	#set the number of grey levels used in the GLCM calculation
+	greyLevels = 16
+	scaler = int(256/greyLevels)
+
+	xres = 288
+	yres = 352
+
+	# extract R and B band individually from image
+	blueBand  = np.zeros([yres,xres])
+	greenBand = np.zeros([yres,xres])
+	redBand   = np.zeros([yres,xres])
+
+	for i in range (0,yres):
+		for j in range (0,xres):
+			# check whether on pixel is part of mask
+			# if pixel is masked, do nothing
+			if maskedImg[i,j,0] == 0 and maskedImg[i,j,1] == 0 and maskedImg[i,j,2] == 0:
+				pass
+			else:
+				# i = ypixel, j = xpixel, 0,2 = blue, red
+				blueBand[i,j]  = int(maskedImg[i,j,0]/scaler)
+				greenBand[i,j] = int(maskedImg[i,j,1]/scaler)
+				redBand[i,j]   = int(maskedImg[i,j,2]/scaler)
+
+	# SPECTRAL FEATURES
+
+	# mean (R and B)
+
+	# standard deviation (B)
+
+	# skewness (B)
+
+	# difference (R-G, R-B and G-B)
+
+	# TEXTURAL FEATURES
+
+	# Grey Level Co-occurrence Matrices (GLCM)
+	GLCM = calculateGLCM(blueBand, greyLevels)
 
 	# Energy (B)
 
