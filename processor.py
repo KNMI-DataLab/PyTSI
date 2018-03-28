b475649abbba49563439ebdf912e6afab976a031
Job Mos <jobmos95@gmail.com>
2018-03-28 15:12:07 +0200

    added filename as input

--- processor.py
+++ processor.py
@@ -22,40 +22,47 @@ from setthresholds import setThresholds
 from calculateintensity import calculateIntensity
 from performstatisticalanalysis import performStatisticalAnalysis
 
-def processor(img, azimuth):
+def processor(img, azimuth, filename):
 	#cv2show(img,"Original image")
 
 	# resolution of the image ( 352x288(x3) ) 
 	#print(img.shape)
 
 	# create mask
+	print('create mask')
 	mask = createmask(img, azimuth)
 
 	# apply the mask and display the result
+	print('apply mask')
 	maskedImg = cv2.bitwise_and(img, mask)
 
 	# plot the overview showing the image, mask, and histogram
 	#overviewPlot(img,mask,maskedImg)
 
 	# set thresholds for plotting and sky cover calculations
+	print('set thresholds')
 	sunnyThreshold,thinThreshold = setThresholds()
 
 	# calculate red/blue ratio per pixel
+	print('calculate rb rabio')
 	redBlueRatio = calculateRatio(maskedImg)
 
 	# calculate the intensity values
 	#intensityValues = calculateIntensity(maskedImg)
 
 	# plot the reb/blue ratios
-	#plotRatio(img,redBlueRatio, sunnyThreshold, thinThreshold)
+	print('plot sky cover image')
+	plotRatio(img,redBlueRatio, sunnyThreshold, thinThreshold, filename)
 
 	# calculate solid angle corrections
 	#calculateSACorrections(...)
 
 	# calculate fractional skycover
+	print('calcualte fSC')
 	fractionalSkyCover = calculateSkyCover(redBlueRatio, sunnyThreshold, thinThreshold)
 
 	# calculates statistical properties of the image
+	print('calculate statistics')
 	energy, entropy, contrast, homogeneity = performStatisticalAnalysis(maskedImg)
 
 	return fractionalSkyCover, energy, entropy, contrast, homogeneity
