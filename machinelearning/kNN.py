2cc29f1beff50fb89195864eee5be748de3d0f15
Job Mos <jobmos95@gmail.com>
2018-04-12 10:53:14 +0200

    LOOCV method can now be tested for ranges of k

--- machinelearning/kNN.py
+++ machinelearning/kNN.py
@@ -3,18 +3,30 @@ from sklearn import preprocessing, model_selection, neighbors
 from sklearn.model_selection import LeaveOneOut
 import pandas as pd
 import sys
+from tqdm import tqdm
 
-df = pd.read_csv('/home/mos/Documents/TSI/machinelearning/data_tsi_32levels.csv')
+df = pd.read_csv('/home/mos/Documents/TSI/machinelearning/data_tsi_256levels.csv')
 df.replace(',0.0', -99999, inplace=True)
 df.drop(['filename'], 1, inplace=True)
 
 X = np.array(df.drop(['cloudClass'],1))
 y = np.array(df['cloudClass'])
 
-X_train, X_test, y_train, y_test = model_selection.train_test_split(X,y,test_size=0.1)
+m=3
+n=len(X)
 
-clf = neighbors.KNeighborsClassifier(n_neighbors=9)
-clf.fit(X_train, y_train)
+# loop over number of n_neighbors
+    # loop over m for averaging
+        # loop over LOOCV method
 
-accuracy = clf.score(X_test, y_test)
-print(accuracy)
+for k in tqdm(range(3,17,2)):
+    sum=0
+    for i in range(0,m):
+        for j in range(0,n):
+            X_train, X_test, y_train, y_test = model_selection.train_test_split(X,y,test_size=1)
+            clf = neighbors.KNeighborsClassifier(n_neighbors=k, algorithm='ball_tree')
+            clf.fit(X_train, y_train)
+            accuracy = clf.score(X_test, y_test)*100
+            sum += accuracy
+    avg = sum/(m*n)
+    print('k =',k,', LOOCV average accuracy =',avg,'%')
